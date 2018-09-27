89ac676a739b1322ce0948f80ae1e5b909e3b1d5
Lukas Loesche <lukas@mesosphere.io>
2018-09-27 16:55:59 +0200

    Automatic update of variable descriptions

--- outputs.tf
+++ outputs.tf
@@ -1,79 +1,95 @@
 # Number of Instance
 output "num_masters" {
+  description = "Specify the amount of masters. For redundancy you should have at least 3"
   value = "${var.num_masters}"
 }
 
 # Cluster Name
 output "name_prefix" {
+  description = "Cluster Name"
   value = "${var.name_prefix}"
 }
 
 # Instance Type
 output "machine_type" {
+  description = "machine type"
   value = "${var.machine_type}"
 }
 
 # Element by zone list
 output "zone_list" {
+  description = "zone list"
   value = ["${var.zone_list}"]
 }
 
 # Source image to boot from
 output "image" {
+  description = "image"
   value = "${var.image}"
 }
 
 # Disk Type to Leverage
 output "disk_type" {
+  description = "Disk Type to Leverage. The GCE disk type. Can be either 'pd-ssd', 'local-ssd', or 'pd-standard'. (optional)"
   value = "${var.disk_type}"
 }
 
 # Disk Size in GB
 output "disk_size" {
+  description = "disk size"
   value = "${var.disk_size}"
 }
 
 # Instance Subnetwork Name
 output "master_subnetwork_name" {
+  description = "master subnetwork name"
   value = "${var.master_subnetwork_name}"
 }
 
 # Customer Provided Userdata
 output "user_data" {
+  description = "User data to be used on these instances (cloud-init)"
   value = "${var.user_data}"
 }
 
 # SSH User
 output "ssh_user" {
+  description = "ssh user"
   value = "${module.dcos-master-instances.ssh_user}"
 }
 
 # SSH Public Key
 output "public_ssh_key" {
+  description = "public ssh key"
   value = "${var.public_ssh_key}"
 }
 
 # Private IP Addresses
 output "private_ips" {
+  description = "List of private ip addresses created by this module"
   value = ["${module.dcos-master-instances.private_ips}"]
 }
 
 # Public IP Addresses
 output "public_ips" {
+  description = "List of public ip addresses created by this module"
   value = ["${module.dcos-master-instances.public_ips}"]
 }
 
 # Tested DCOS OSes Name
 output "dcos_instance_os" {
+  description = "Operating system to use. Instead of using your own AMI you could use a provided OS."
   value = "${var.dcos_instance_os}"
 }
 
 # Master Node Self Link
 output "instances_self_link" {
+  description = "List of instance self links"
   value = ["${module.dcos-master-instances.instances_self_link}"]
 }
 
 # Returns the ID of the prereq script (if images are not used)
 output "prereq_id" {
+  description = "prereq id"
   value = "${module.dcos-master-instances.prereq_id}"
 }
--- variables.tf
+++ variables.tf
@@ -1,56 +1,81 @@
 # number of instance
-variable "num_masters" {}
+variable "num_masters" {
+  description = "Specify the amount of masters. For redundancy you should have at least 3"
+}
 
 # Cluster Name
-variable "name_prefix" {}
+variable "name_prefix" {
+  description = "Cluster Name"
+}
 
 # DCOS version
-variable "dcos_version" {}
+variable "dcos_version" {
+  description = "Specifies which DC/OS version instruction to use. Options: 1.9.0, 1.8.8, etc. See dcos_download_path or dcos_version tree for a full list."
+}
 
 # Instance Type
-variable "machine_type" {}
+variable "machine_type" {
+  description = "machine type"
+}
 
 # Element by zone list
 variable "zone_list" {
+  description = "zone list"
   type    = "list"
   default = []
 }
 
 # Source image to boot from
-variable "image" {}
+variable "image" {
+  description = "image"
+}
 
 # Disk Type to Leverage
-variable "disk_type" {}
+variable "disk_type" {
+  description = "Disk Type to Leverage. The GCE disk type. Can be either 'pd-ssd', 'local-ssd', or 'pd-standard'. (optional)"
+}
 
 # Disk Size in GB
-variable "disk_size" {}
+variable "disk_size" {
+  description = "disk size"
+}
 
 # Instance Subnetwork Name
-variable "master_subnetwork_name" {}
+variable "master_subnetwork_name" {
+  description = "master subnetwork name"
+}
 
 # Customer Provided Userdata
 variable "user_data" {
+  description = "User data to be used on these instances (cloud-init)"
   default = ""
 }
 
 # SSH User
-variable "ssh_user" {}
+variable "ssh_user" {
+  description = "ssh user"
+}
 
 # SSH Public Key
-variable "public_ssh_key" {}
+variable "public_ssh_key" {
+  description = "public ssh key"
+}
 
 # Add special tags to the resources created by this module
 variable "tags" {
+  description = "Add custom tags to all resources"
   type    = "list"
   default = []
 }
 
 # Format the hostname inputs are index+1, region, name_prefix
 variable "hostname_format" {
+  description = "Format the hostname inputs are index+1, region, cluster_name"
   default = "%[2]s-masters-%[1]d"
 }
 
 # The operating system to use. Instead of using your own AMI you could use a provided OS.
 variable "dcos_instance_os" {
+  description = "Operating system to use. Instead of using your own AMI you could use a provided OS."
   default = "centos_7.4"
 }
